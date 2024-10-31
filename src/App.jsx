09d5348ea12fbfa9e196463ee7eb3d1db58fbd97
Harishanth03 <kandeepanharishanth@gmail.com>
2024-10-31 12:30:52 +0530

    Digital Clock finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
 import { useState } from 'react'
 import './App.css'
+import { useEffect } from 'react';
 
 function App() {
 
   const [currentTime , setCurrentTime] = useState(new Date);
+
+  useEffect(() => {
+
+    const timer = setInterval(() => {
+
+      setCurrentTime(new Date);
+
+    }, 1000);
+
+    return() => clearInterval(timer);
+
+  },[]);
+
+  const formatHour = (hour) => {
+
+    return hour === 0 ? 12 : hour > 12 ? hour - 12 : hour;
+
+  }
+
+  const formatTimeWithero = (num) => {
+
+    return num < 10 ? `0${num}` : num;
+
+  };
+
+  const formatDate = (date) => {
+
+    const Option = {weekday : "long" , year : "numeric" , month : "long" , day : "numeric"}
+
+    return date.toLocaleDateString(undefined , Option);
+
+  }
 
   return (
     <>
@@ -11,9 +44,9 @@ function App() {
 
         <h1>Digital Clock</h1>
 
-        <div className="time">11:59:59 Am</div>
+        <div className="time">{formatTimeWithero(formatHour(currentTime.getHours()))} : {formatTimeWithero(currentTime.getMinutes())} : {formatTimeWithero(currentTime.getSeconds())}</div>
 
-        <div className="date">Thursday , October 31, 2024</div>
+        <div className="date">{formatDate(currentTime)}</div>
       </div>
     </>
   )
